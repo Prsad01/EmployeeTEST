a255c2b6f502be86b926d4b6e72bb3b79c3d7cb7
Prsad01 <149363594+Prsad01@users.noreply.github.com>
2025-01-06 14:24:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app =  FastAPI()
 @app.get('/')
 def index():
     return {'msg':'success'}
+
+if __name__ == "__main__":
+    print("Starting webserver...")
+    uvicorn.run(
+        api,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 8080)),
+        debug=os.getenv("DEBUG", False),
+        log_level=os.getenv('LOG_LEVEL', "info"),
+        proxy_headers=True
+    )
